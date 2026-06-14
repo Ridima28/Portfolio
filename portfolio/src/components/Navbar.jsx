4cb486485cd41ccea3c22d2d18f8fff80393867e
Ridima28 <ridimafinal@gmail.com>
2026-06-14 19:51:53 +0530

    finishing off

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -10,7 +10,6 @@ export const Navbar = () => {
         { name: 'Home', link: '#home' },
         { name: 'About', link: '#about' },
         { name: 'Skills', link: '#skills' },
-        { name: 'Projects', link: '#projects' },
         { name: 'Contact', link: '#contact' },
     ];
 
